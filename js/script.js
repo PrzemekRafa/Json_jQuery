e6c837659594a5d95bfe59d41c90cb8cc0af38c6
Przemek Rafa Mac <przemyslaw.rafa@gmail.com>
2017-07-15 13:51:04 +0200

    fin

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 'use strict';
 
 $(function() {
-    $.getJSON('https://jsonplaceholder.typicode.com/users/1', function (data) {
-        console.log(data);
-        console.log(data.name);
-        console.log(data.address.street);
+    
+    function getUsers (init) {
         
-        var parName = document.createElement('p');
-        var parStreet = document.createElement('p');
-        
-        parName.innerHTML = data.name;
-        parStreet.innerHTML = data.address.street;
-        
-        $('#users-data').append(parName);
-        $('#users-data').append(parStreet);
-        
-    });
+        for (var i=init; i <= init+4; i++) {
+
+                $.getJSON('https://jsonplaceholder.typicode.com/users/' + i, function (data) {
+            //            console.log(data);
+            //            console.log(data.name);
+            //            console.log(data.address.street);
+
+                    var parId = document.createElement('p');
+                    var parName = document.createElement('p');
+                    var parStreet = document.createElement('p');
+
+                    parId.className += 'user-id';
+                    parName.className += 'user-name';
+                    parStreet.className += 'user-street-name';
+
+                    parId.innerHTML = 'Id użytkownika ' + data.id;
+                    parName.innerHTML = 'Imię i nazwisko ' + data.name;
+                    parStreet.innerHTML = 'Ulica ' + data.address.street;
+
+                    $('#users-data').append(parId);
+                    $('#users-data').append(parName);
+                    $('#users-data').append(parStreet);
+
+                });
+            }
+    }
+    
+    getUsers(1);
+    
+    $('#load-users').on('click', function() {
+//        console.log('test');
+        var usersLoaded = $('.user-name').length;
+        console.log(usersLoaded);
+        getUsers(usersLoaded + 1);
+    })
+    
 });
