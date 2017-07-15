3cb33c09c23a5b3c3f9c0612f3c97fef9f381db2
Przemek Rafa Mac <przemyslaw.rafa@gmail.com>
2017-07-15 12:00:09 +0200

    jeden_user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+$(function() {
+    $.getJSON('https://jsonplaceholder.typicode.com/users/1', function (data) {
+        console.log(data);
+        console.log(data.name);
+        console.log(data.address.street);
+        
+        var parName = document.createElement('p');
+        var parStreet = document.createElement('p');
+        
+        parName.innerHTML = data.name;
+        parStreet.innerHTML = data.address.street;
+        
+        $('#users-data').append(parName);
+        $('#users-data').append(parStreet);
+        
+    });
+});
